25f3c9e8a6fa1f2944a6f294e285a0df441a6056
mwalawski <michal.walawski@gmail.com>
2019-11-09 00:08:49 +0100

    Changes in storing data. Minor code improvements

--- collect_order.py
+++ collect_order.py
@@ -1,49 +1,61 @@
-menu = ['B', 'BBQ', 'CL', 'CH', 'CHB']
-menu_order = {item: 0 for item in menu}
-orders = {}
+import sqlite3
+
+conn = sqlite3.connect('restaurant.db')
+c = conn.cursor()
+
+
+"""-----------------------------------------------------------------------------------------"""
+
+
+class Customer:
+    pass
+
+
+"""-----------------------------------------------------------------------------------------"""
 
 
 class Order:
-	id = 0
+    id = 0
 
-	def __init__(self, hour=None, food=None, phone=None):
-		self.hour = hour
-		self.food = food
-		self.phone = phone
-		self.id = Order.id
-		Order.id += 1
+    def __init__(self, hour=None, food=None, phone=None):
+        self.hour = hour
+        self.food = food
+        self.phone = phone
+        self.id = Order.id
+        Order.id += 1
+
+
+def create_table():
+    c.execute('CREATE TABLE IF NOT EXISTS orders(ID REAL, product REAL, amount REAL, hour TEXT, phone TEXT)')
+
+
+def dynamic_data_entry(id, product, amount, hour, phone):
+    create_table()
+    c.execute("INSERT INTO orders(ID, product, amount, hour, phone) VALUES (?,?,?,?,?)", (id, product, amount, hour, phone))
+    conn.commit()
 
 
 def pick_order():
-	print("Hello! Your order will be collected in 2 steps:",sep='\n')
-	print("First, type name of your burger and then, give the amount.\n")
-	print("Each bun has own name tag given below",sep='\n')
-	print("CL - Classic","CH - Cheeseburger","CHB - Cheese&Bacon","B - Bacon","BBQ - Barbecue\n",sep='\n')
-	tag = input("Please input burger tag: ")
-	amount = input("Give the amount: ")
-	menu_order[tag] = amount
-	phone = input("Great! Please enter phone number: ")
-	hour = input("What is estimated time of your arrival?: ")
-	order = Order(hour, menu_order, phone)
-	orders[order.id]=dict(order=order.food,phone=order.phone,hour=order.hour)
+    order = Order()
 
-	print(orders)
-	test=input("To make another order type Y (Yes): ")
-	if test=="Y":
-		pick_order()
-	else:
-		print("See you at {}".format(order.hour))
+    print("Hello! Your order will be collected in 2 steps:")
+    print("First, type name of your burger and then, give the amount.\n")
+    print("Each bun has own name id given below")
+    print("1 - Classic", "3 - Cheeseburger", "6 - Cheese&Bacon", "2 - Bacon", "4 - BBQ\n", sep='\n')
+    tag = int(input("Please input burger id: "))
+    amount = input("Give the amount: ")
+    order.phone = input("Great! Please enter phone number: ")
+    order.hour = input("What is estimated time of your arrival?: ")
 
-	print(dir(order))
+    dynamic_data_entry(order.id, tag, amount, order.hour, order.phone)
+
+    test = input("To make another order type Y (Yes): ")
+    if test == "Y":
+        pick_order()
+    else:
+        print("See you at {}".format(order.hour))
+        c.close()
+        conn.close()
+
+
 pick_order()
-
-# #counttest:
-# order=Order()
-# print(order.id)
-
-
-# from datetime import timedelta
-
-# #ordertimetable
-# for i in range(0,465,15):
-# 	print([str(timedelta(minutes=810+i))[:-3],0,0,0,0,0,0,0,0])
