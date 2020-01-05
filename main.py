d329f512a7023f7450a0fef2eeb92ebf7fddd255
mwalawski <michal.walawski@gmail.com>
2020-01-05 14:01:05 +0100

    Resolved modules import issue. Added app favicon.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from os import path
-from interface import *
-from database import *
+import tkinter as tk
+from interface import Interface
+
 
 class App(Interface):
 	def __init__(self):
+		Interface.__init__(self)
 		self.root = tk.Tk()
-		self.root.title('Food Order')
+		self.root.title(' Food Order')
+		self.root.iconbitmap('images/favicon.ico')
 		self.root.config(bg='#454c54')
 		self.create_top_bar()
 		self.create_menu_bar()
@@ -15,6 +17,4 @@ class App(Interface):
 
 
 if __name__ == '__main__':
-	if not path.exists('orders.db'):
-		create = Database()
 	run = App()
